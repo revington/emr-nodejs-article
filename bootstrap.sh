b43421f26062693c991f24b009e666da1d56b3d7
Pedro Narciso García Revington <p.revington@gmail.com>
2016-11-23 15:50:33 +0100

    some fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 NODE_VERSION=6.9.1
 GITHUB_USER='revington'
-GITHUB_REPO='node-emr-article'
+GITHUB_REPO='emr-nodejs-article'
 
 function install {
   # Installs the project with already built in dependencies
   local install_dir="/opt/$GITHUB_REPO"
   sudo mkdir -p "$install_dir"
-  curl -SL "https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/tarball/master" | sudo tar xz -C $install_dir --strip-components=1
+  curl -vSL "https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/tarball/master" | sudo tar xz -C $install_dir --strip-components=1
   pushd "$install_dir"
-  npm install
+  /usr/local/bin/npm install -g .
   popd
 }
 
