e3d22f60a453b18117b12d8095a125b768718897
Pedro Narciso García Revington <p.revington@gmail.com>
2016-11-23 13:27:33 +0100

    small changes

---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+NODE_VERSION=6.9.1
+GITHUB_USER='revington'
+GITHUB_REPO='node-emr-article'
+
+function install {
+  # Installs the project with already built in dependencies
+  local install_dir="/opt/$GITHUB_REPO"
+  sudo mkdir -p "$install_dir"
+  curl -SL "https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/tarball/master" | sudo tar xz -C $install_dir --strip-components=1
+  pushd "$install_dir"
+  npm install
+  popd
+}
+
+# Install node
+curl -SL "https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-x64.tar.xz" | sudo tar -xJ -C /usr/local --strip-components=1 
+
+install
