cc900913164f631cb75e843b0302117f6dca59fc
Pedro Narciso García Revington <p.revington@gmail.com>
2016-11-26 17:25:48 +0100

    format

--- add-step.sh
+++ add-step.sh
@@ -3,4 +3,6 @@ CLUSTER_ID=$1
 BUCKET='pngr-emr-demo'
 OUTPUT='output/1'
 
-aws emr add-steps --cluster-id $CLUSTER_ID --steps Name=CountWords,Type=Streaming,Args=[-input,s3://$BUCKET/input,-output,s3://$BUCKET/$OUTPUT,-mapper,mapper,-reducer,reducer] 
+aws emr add-steps \
+ --cluster-id $CLUSTER_ID \
+ --steps Name=CountWords,Type=Streaming,Args=[-input,s3://$BUCKET/input,-output,s3://$BUCKET/$OUTPUT,-mapper,mapper,-reducer,reducer] 
